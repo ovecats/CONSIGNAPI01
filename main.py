5c45adc5779d0ad65821699cc79938fb15602644
ovecats <datc23@gmail.com>
2022-09-15 21:53:02 -0500

    avance2

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ app = FastAPI()
 
 @app.get('/home')
 def home():
-    return {"message": "2022"}
+    return {"message": "consigna"}
 
 @app.get('/Año con más carreras')
 def Fecha():
-    return {"message": "2022"}
+    return {"message": "2021, 23 carreras"}
 
 @app.get('/Piloto con mayor cantidad de primeros puestos')
 def name():
-    return {"message": "2022"}
+    return {"message": "hamilton"}
 
 @app.get('/Nombre del circuito más corrido')
 def pista():
-    return {"message": "2022"}
+    return {"message": "nazionale di monza"}
 
 @app.get('/Piloto con mayor cantidad de puntos en total, cuyo constructor sea de nacionalidad sea American o British')
 def nacionalidad():
-    return {"message": "2022"}
+    return {"message": "jenson button 1154"}
 
 @app.get('/otros')
 def end():
